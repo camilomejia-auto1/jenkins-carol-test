822d4cd0b2d21780aabd51d0966238102420f584
Camilo Mejia <camilo.mejia@auto1.com>
2022-04-22 15:07:52 +0200

    Remove lib config

--- jobs/create_project.groovy
+++ jobs/create_project.groovy
@@ -1,24 +1,4 @@
 folder('refurbishment') {
   displayName('Refurbishment')
   description("Folder for refurbishment's jobs")
-  properties {
-    folderLibraries {
-      libraries {
-        libraryConfiguration {
-          name('refurbishment')
-          implicit(false)
-          retriever {
-            scmSourceRetriever {
-              scm {
-                gitSCMSource {
-                  remote("git@github.com:camilomejia-auto1/jenkins-carol-test")
-                }
-              }
-            }
-          }
-          defaultVersion('master')
-        }
-      }
-    }
-  }
 }
