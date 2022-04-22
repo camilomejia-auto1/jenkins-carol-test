6276c360fa2d7980d45b6ab4c68fdf6190ec6322
Camilo Mejia <camilo.mejia@auto1.com>
2022-04-22 15:18:09 +0200

    '

--- jobs/app.groovy
+++ jobs/app.groovy
@@ -1,5 +1,27 @@
 folder('refurbishment') {
-  displayName('Refurbishment 2')
+  displayName('Refurbishment 3')
+  description("Folder for refurbishment's jobs")
+
+  properties {
+    folderLibraries {
+      libraries {
+        libraryConfiguration {
+          name('refurbishment')
+          implicit(false)
+          retriever {
+            scmSourceRetriever {
+              scm {
+                gitSCMSource {
+                  remote("camilomejia-auto1/jenkins-carol-test")
+                }
+              }
+            }
+          }
+          defaultVersion('master')
+        }
+      }
+    }
+  }
 }
 
 pipelineJob('refurbishment/carol') {
