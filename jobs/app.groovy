210125c492213008517e47eda39154d7ace3a09d
Camilo Mejia <camilo.mejia@auto1.com>
2022-04-22 12:58:02 +0200

    Fix triggers

--- jobs/app.groovy
+++ jobs/app.groovy
@@ -5,22 +5,28 @@ folder('refurbishment') {
 
 pipelineJob('refurbishment/carol') {
   displayName('Carol app')
-  triggers {
-    githubPullRequest {
-      useGitHubHooks()
-      permitAll()
-      displayBuildErrorsOnDownstreamBuilds()
-      extensions {
-        commitStatus {
-          context('jenkins')
-          triggeredStatus('starting PR build of statistics-service')
-          startedStatus('started PR build...')
-          completedStatus('SUCCESS', 'Click on Details to see changes')
-          completedStatus('FAILURE', 'Something went wrong')
-          completedStatus('PENDING', 'Still in progress...')
-          completedStatus('ERROR', 'Something went really wrong')
+
+  properties {
+    pipelineTriggers {
+      triggers {
+        githubPullRequest {
+          useGitHubHooks()
+          permitAll()
+          displayBuildErrorsOnDownstreamBuilds()
+          extensions {
+            commitStatus {
+              context('jenkins')
+              triggeredStatus('starting PR build of statistics-service')
+              startedStatus('started PR build...')
+              completedStatus('SUCCESS', 'Click on Details to see changes')
+              completedStatus('FAILURE', 'Something went wrong')
+              completedStatus('PENDING', 'Still in progress...')
+              completedStatus('ERROR', 'Something went really wrong')
+            }
+          }
         }
       }
     }
   }
+  
 }
